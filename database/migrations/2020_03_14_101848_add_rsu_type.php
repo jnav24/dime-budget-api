d072b068533b3875ec57d51c303a9ff976a235ff
Justin Navarro <info@justinnavarro.net>
2021-02-03 23:06:03 -0500

    brought in old migrations

---
+++ database/migrations/2020_03_14_101848_add_rsu_type.php
@@ -0,0 +1,32 @@
+<?php
+
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddRsuType extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+        DB::table('investment_types')->insert([
+            ['name' => 'Restricted Stock', 'slug' => 'rsu', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
