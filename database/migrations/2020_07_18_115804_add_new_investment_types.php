a8e83bf589c6855b55e2b4532b0f7f17fd3b655b
Justin Navarro <info@justinnavarro.net>
2021-02-04 00:29:05 -0500

    add migrations

--- database/migrations/2020_07_18_115804_add_new_investment_types.php
+++ database/migrations/2020_07_18_115804_add_new_investment_types.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddInvestmentTypes extends Migration
+class AddNewInvestmentTypes extends Migration
 {
     /**
      * Run the migrations.
@@ -14,7 +14,7 @@ class AddInvestmentTypes extends Migration
      */
     public function up()
     {
-        DB::table('investment_types')->insert([
+        DB::table('investment_types')->insertOrIgnore([
             ['name' => 'Annuities', 'slug' => 'rent', 'created_at' => \Carbon\Carbon::now(), 'updated_at' => \Carbon\Carbon::now()],
             ['name' => 'Bonds', 'slug' => 'rent', 'created_at' => \Carbon\Carbon::now(), 'updated_at' => \Carbon\Carbon::now()],
             ['name' => 'Certificate of Deposit (CD)', 'slug' => 'cd', 'created_at' => \Carbon\Carbon::now(), 'updated_at' => \Carbon\Carbon::now()],
