e3e11844b21a666dfb254dad98704620d15d07a2
Justin Navarro <info@justinnavarro.net>
2021-02-07 15:28:28 -0500

    created logout listener

---
+++ app/Listeners/LogoutListener.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Listeners;
+
+use Illuminate\Auth\Events\Logout;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+
+class LogoutListener
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param Logout $event
+     * @return void
+     */
+    public function handle(Logout $event)
+    {
+        $tokenId = request()->bearerToken();
+        $user = $event->user;
+
+        if (!empty($user) && !empty($tokenId)) {
+            $user->tokens()->where('id', $tokenId)->delete();
+        }
+    }
+}
