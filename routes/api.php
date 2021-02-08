4b25085d4eda9ee89c916d6696cc4be2f3bd46ff
Justin Navarro <info@justinnavarro.net>
2021-02-08 12:39:24 -0500

    update the routes and rename controller methods to be more readable

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BudgetController;
+use App\Http\Controllers\BudgetAggregationController;
+use App\Http\Controllers\BudgetTemplateController;
+use App\Http\Controllers\SearchController;
+use App\Http\Controllers\TypeController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,23 +21,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware(['auth:sanctum', 'verify.session'])->group(function () {
-    Route::get('budget-aggregate', [\App\Http\Controllers\BudgetAggregationController::class, 'getYearlyAggregation']);
-    Route::get('current-budget-aggregate/{year}', [\App\Http\Controllers\BudgetAggregationController::class, 'getSingleYearAggregation']);
-    Route::get('unpaid-aggregate', [\App\Http\Controllers\BudgetAggregationController::class, 'getCountOfUnPaidBills']);
-
-    Route::get('budgets', [\App\Http\Controllers\BudgetController::class, 'getAllBudgets']);
-    Route::get('budgets/{id}', [\App\Http\Controllers\BudgetController::class, 'getSingleBudgetExpenses']);
-    Route::post('budgets', [\App\Http\Controllers\BudgetController::class, 'saveBudget']);
-    Route::delete('budgets/{id}', [\App\Http\Controllers\BudgetController::class, 'deleteBudget']);
-    Route::delete('budget-expense/{id}', [\App\Http\Controllers\BudgetController::class, 'deleteBudgetExpense']);
-
-    Route::get('budget-templates', [\App\Http\Controllers\BudgetTemplateController::class, 'getAllBudgetTemplates']);
-    Route::post('budget-templates', [\App\Http\Controllers\BudgetTemplateController::class, 'saveBudgetTemplates']);
-    Route::delete('budget-templates', [\App\Http\Controllers\BudgetTemplateController::class, 'deleteBudgetTemplate']);
-
-    Route::post('search', [\App\Http\Controllers\SearchController::class, 'runSearch']);
-
-    Route::get('types/bill', [\App\Http\Controllers\TypeController::class, 'bill']);
-
-    Route::get('user-profile', [\App\Http\Controllers\UserController::class, 'updateUserProfile']);
+    Route::get('budget-aggregate', [BudgetAggregationController::class, 'getYearlyAggregation']);
+    Route::get('current-budget-aggregate/{year}', [BudgetAggregationController::class, 'getSingleYearAggregation']);
+    Route::get('unpaid-aggregate', [BudgetAggregationController::class, 'getCountOfUnPaidBills']);
+    Route::resource('budgets', BudgetController::class);
+    Route::delete('budget-expense/{id}', [BudgetController::class, 'deleteBudgetExpense']);
+    Route::resource('budget-templates', BudgetTemplateController::class);
+    Route::post('search', [SearchController::class, 'runSearch']);
+    Route::get('types/bill', [TypeController::class, 'bill']);
+    Route::get('user-profile', [UserController::class, 'updateUserProfile']);
 });
